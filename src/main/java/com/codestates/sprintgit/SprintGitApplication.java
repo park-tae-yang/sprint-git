b68cc03ce2594f48363a9bae7765645bd650e03d
park-tae-yang <pxodid2000@gmail.com>
2023-06-13 18:16:32 +0900

    initial

---
+++ src/main/java/com/codestates/sprintgit/SprintGitApplication.java
@@ -0,0 +1,13 @@
+package com.codestates.sprintgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintGitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codestates/sprintgit/SprintGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codestates.sprintgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
